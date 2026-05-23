d015bf9f69677074763f3566729ec832470aacbd
BesuHosiso <besuyeboss@gmail.com>
2026-05-23 21:47:58 +0300

    feat(StatsPage): takes in two components & executes them

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,8 +1,16 @@
+import StatCards from "../components/features/StatCards";
+import TagBreakdown from "../components/features/TagBreakdown";
+
+
 const Stats = () => {
     return (
-        <div>
-            <h1>Stats</h1>
-            <p>This is the Stats page.</p>
+        <div className="flex flex-col gap-4">
+            <h1 className="text-2xl font-bold text-white">Your Growth Journey</h1>
+            <StatCards />
+            <div className="mt-6">
+                <h2 className="text-lg font-semibold text-white mb-2">What You Are Learning</h2>
+                <TagBreakdown />
+            </div>
         </div>
     )
 }

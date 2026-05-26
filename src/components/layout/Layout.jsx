2455be5441f7155ffeabc8c072bce99a861e2bc5
BesuHosiso <besuyeboss@gmail.com>
2026-05-26 20:50:56 +0300

    refactor(layout): use flexbox and --sidebar-width variable to prevent sidebar overlay

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,23 +7,24 @@ const Layout = ({ children }) => {
   const location = useLocation();
 
   const isFocusPage = location.pathname === '/focus';
+  const sidebarWidth = isOpen ? '20%' : '56px';
 
   return (
-    <div className="min-h-screen bg-night flex flex-col md:flex-row">
-      <div
-        className={`
-          transition-all duration-300
-          ${isFocusPage ? 'md:fixed md:inset-y-0 md:left-0 md:z-50 md:w-[20%] md:opacity-0 md:hover:opacity-100' : 'md:w-[20%] md:shrink-0 relative'}
-        `}
-      >
+    <div 
+      className="min-h-screen bg-night flex flex-col md:flex-row"
+      style={{ '--sidebar-width': isFocusPage ? '0px' : sidebarWidth }}
+    >
+      <div className={`
+        shrink-0 transition-[width] duration-300 ease-in-out z-40
+        ${isFocusPage ? 'md:fixed md:inset-y-0 md:left-0 md:w-[20%] md:opacity-0 md:hover:opacity-100' : 'md:w-[var(--sidebar-width)]'}
+      `}>
         <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
       </div>
-      
-      <main
+
+     <main
         className={`
-          flex-1 p-4 pt-16 md:pt-6 min-w-0
+          flex-1 px-8 py-4 pt-16 md:pt-6 min-w-0
           transition-all duration-300 ease-in-out
-          ${isFocusPage ? 'md:pl-0' : (isOpen ? 'md:pl-0' : 'md:pl-0')}
         `}
       >
         {children}

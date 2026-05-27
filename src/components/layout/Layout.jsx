d21dce647b0c9be525c2052359f9bb3d7d1dd8f2
BesuHosiso <besuyeboss@gmail.com>
2026-05-27 12:17:03 +0300

    refactor(Layout): add mobileHeader component for mobile nav & fix focus page sidebar activation

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import MobileHeader from './MobileHeader';
 import Sidebar from './SideBar';
 
 const Layout = ({ children }) => {
@@ -15,15 +16,18 @@ const Layout = ({ children }) => {
       className="min-h-screen bg-night flex flex-col md:flex-row"
       style={{ '--sidebar-width': isFocusPage ? '0px' : sidebarWidth }}
     >
+      {/* Mobile/Tablet Header (visible on screens smaller than 'lg') */}
+      <MobileHeader />
+
       <div className={`
         shrink-0 transition-[width,opacity] duration-300 ease-in-out z-40 bg-surface
-        ${isFocusPage ? 'md:fixed md:inset-y-0 md:left-0 md:w-[64px] md:opacity-0 md:hover:opacity-100' : 'hidden md:block w-full md:w-[var(--sidebar-width)]'}
+        ${isFocusPage ? 'lg:fixed lg:inset-y-0 lg:left-0 lg:w-[64px] lg:opacity-0 lg:hover:opacity-100' : 'hidden lg:block w-full lg:w-[var(--sidebar-width)]'}
       `}>
         <Sidebar isOpen={isFocusPage ? false : isOpen} setIsOpen={setIsOpen} />
       </div>
 
      <main
-        className="flex-1 px-8 py-4 pt-16 md:pt-6 min-w-0 transition-all duration-300 ease-in-out"
+        className="flex-1 px-8 py-4 pt-16 lg:pt-6 lg:pl-[var(--sidebar-width)] min-w-0 transition-all duration-300 ease-in-out"
       >
         {children}
       </main>
